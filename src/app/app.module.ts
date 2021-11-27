17704ec7cc367cfe892278c961291310ef42f369
Sarah <sarah@tomorrow.services>
2021-11-27 17:01:42 +0200

    frontend chenges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,78 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AdminComponent } from './admin/admin.component';
-import { LandingPageComponent } from './landing-page/landing-page/landing-page.component';
-import { SeatListComponent } from './seats/seat-list/seat-list.component';
-import { SeatFormComponent } from './seats/seat-form/seat-form.component';
-import { SeatItemComponent } from './seats/seat-item/seat-item.component';
-import { RegisterComponent } from './register/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    AdminComponent,
-    LandingPageComponent,
-    SeatListComponent,
-    SeatFormComponent,
-    SeatItemComponent,
-    RegisterComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      {
-        path: 'theaters',
-        loadChildren: () => import('./theaters/theaters.module').then((m) => m.TheatersModule),
-      },
-      {
-        path: 'plays',
-        loadChildren: () => import('./plays/plays.module').then((m) => m.PlaysModule),
-      },
-      {
-        path: 'seats',
-        component: SeatListComponent,
-      },
-      {
-        path: 'seats/:id',
-        component: SeatListComponent,
-      },
-      {
-        path: 'login',
-        component: LoginComponent,
-      },
-      {
-        path: 'login/:succ',
-        component: LoginComponent,
-      },
-      {
-        path: 'register',
-        component: RegisterComponent,
-      },
-
-      {
-        path: 'admin',
-        component: AdminComponent,
-      },
-      {
-        path: '',
-        component: LandingPageComponent,
-      },
-      {
-        path: '**',
-        redirectTo: '',
-        pathMatch: 'full',
-      },
-    ]),
-    FontAwesomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/plays/shared/play.model.ts
+++
@@ -1,9 +0,0 @@
-import { Guid } from "guid-typescript";
-import { Theater } from "src/app/theaters/shared/theater.model";
-export class Play {
-    id?: Guid;
-    title!: string;
-    category!: string;
-    duration!: number;
-    theaterId?: Guid;
-}
